7048c58f28d8cf11a6285d50c18826f619b3a9cc
yasirghouri <yasir.ag7@gmail.com>
2023-04-14 02:09:18 +0500

    seperating Events in their own class

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,10 @@ import { User } from "./models/User";
 
 const user = new User({ name: "new record", age: 0 });
 
-user.save();
+user.events.on("change", () => {
+  console.log("change #1");
+});
+
+user.events.trigger("change");
 
 console.log("USER", user);
